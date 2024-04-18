e8c4cb1bac02b24ee47762ea71dd5d929895d5b8
Delta O <deltaoliver2@gmail.com>
2024-04-18 16:31:24 +0800

    Basic skeleton structure implemented
    un-stylised skeleton for the template

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ from app import app
 @app.route('/index')
 def index():
     return render_template("index.html")
+
+@app.route('/view')
+def view():
+    return render_template("challengeView.html")
