46f1a3f1019794cab32a906b8cfd6993843b3a09
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-04-21 22:19:16 +0800

    creating new utilities script
    so far contains a conversion from unix time to human time

---
+++ app/utilities.py
@@ -0,0 +1,18 @@
+def secondsToTime(totalSeconds):
+	daysLeft = int(totalSeconds/(60*60*24))
+	hoursLeft = int((totalSeconds - daysLeft*(60*60*24))/3600)
+	minutesLeft = int((totalSeconds - daysLeft*(60*60*24)-hoursLeft*3600)/60)
+	timeString = ""
+	if daysLeft > 0:
+	    timeString +=  str(daysLeft) + " day"
+	    if daysLeft > 1:
+	        timeString += "s"
+	if hoursLeft > 0:
+	    timeString += " "+str(hoursLeft)+" hour"
+	    if hoursLeft > 1:
+	        timeString += "s"
+	if minutesLeft > 0:
+	    timeString += " "+str(minutesLeft)+" minute"
+	    if minutesLeft > 1:
+	        timeString += "s"
+	return timeString.strip()
