f5081f06fb47bf8c9a9aa92f879a7e5b648ca463
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-04-21 09:08:09 +0800

    Create end-points for MVP API

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,66 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def index():
     #example challenge below:
     challenges = [
     {
-        "title": "My Test Challenge",
-        "startArticle": "University",
-        "endArticle": "Bottled Water",
-        "guesses": 6,
-        "creator": "Joseph",
-        "timeLeft": "2 hours 13 minutes"
+        'title': 'My Test Challenge',
+        'startArticle': 'University',
+        'endArticle': 'Bottled Water',
+        'guesses': 6,
+        'creator': 'Joseph',
+        'timeLeft': '2 hours 13 minutes'
     },{
-        "title": "My Test Challenge Again!",
-        "startArticle": "Library",
-        "endArticle": "Wisdom Teeth",
-        "guesses": 1,
-        "creator": "Not Joseph",
-        "timeLeft": "13 minutes"
+        'title': 'My Test Challenge Again!',
+        'startArticle': 'Library',
+        'endArticle': 'Wisdom Teeth',
+        'guesses': 1,
+        'creator': 'Not Joseph',
+        'timeLeft': '13 minutes'
     }]
-    return render_template("index.html", challenges=challenges)
+    return render_template('index.html', challenges=challenges)
+
+@app.route('/create', methods=['GET', 'PUT'])
+def create():
+    #TODO
+    if request.method == "PUT":
+        pass
+    else:
+        pass
+
+#Submit should always include an id parameter
+@app.route('/submit', methods=['GET', 'PUT'])
+def submit():
+    #TODO
+    submission_id = request.args.get("id", default=-1, type=int)
+    if request.method == "PUT":
+        pass
+    else:
+        pass
+    pass
+
+#View should always include an id parameter
+@app.route('/view', methods=['GET'])
+def submit():
+    #TODO
+    #TODO - View should have server-side protection from being viewed by wrong account while challenge is active (not MVP)
+    submission_id = request.args.get("id", default=-1, type=int)
+    print(submission_id)
+    pass
+
+#View should always include an id parameter
+@app.route('/view', methods=['GET'])
+def submit():
+    #TODO
+    #TODO - View should have server-side protection from being viewed by wrong account while challenge is active (not MVP)
+    submission_id = request.args.get("id", default=-1, type=int)
+    print(submission_id)
+    pass
+
+@app.route('/login', methods=["POST"])
+def login():
+    #TODO - unsure of implementation/parameters currently, because of security concerns
+    pass
