b9e798aa4a48d602f4b7238fc32b49db2b9a3f3f
RadiationOcelot <67453536+RadiationOcelot@users.noreply.github.com>
2024-04-26 18:25:56 +0800

    Added + and - button

--- app/static/front-end-scripts/submit.js
+++ app/static/front-end-scripts/submit.js
@@ -10,5 +10,13 @@ function revealPath() {
     }
 }
 
+function removePath() {
+    if (index > 0){
+        index = index - 1;
+        inputs[index].classList.add("formPathHidden");
+    }
+}
+
 
 document.getElementById("revealPathForm").addEventListener("click", revealPath);
+document.getElementById("removePathForm").addEventListener("click", removePath);
