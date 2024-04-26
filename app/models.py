d2de6b0a701a5c0065e972eac5872d2813eb7cc7
RadiationOcelot <67453536+RadiationOcelot@users.noreply.github.com>
2024-04-26 21:24:33 +0800

    Added models to database, initialised database

---
+++ app/models.py
@@ -0,0 +1,38 @@
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+class User(db.Model):
+    # Integer column type with primary_key=True will enable auto-increment
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.Text, nullable=False, unique=True)
+    points = db.Column(db.Integer, nullable=False)
+    # I believe there are more secure ways of implementing this
+    password_hash = db.Column(db.Text, nullable=False)
+    challenges = db.relationship('Challenge', backref='user')
+    submissions = db.relationship('Submission', backref='user')
+    
+    def __repr__(self):
+        return f'User({self.id}, "{self.username}", {self.points}, "{self.password_hash}")'
+
+class Challenge(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    poster_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    title = db.Column(db.Text, nullable=False)
+    path = db.Column(db.Text, nullable=False)
+    # storing datetime as an integer as epoch as the datetime object makes dealing with time zones very frustrating
+    dt_submit = db.Column(db.Integer, nullable=False)
+    dt_finish = db.Column(db.Integer, nullable=False)
+    finished = db.Column(db.Boolean, nullable=False)
+    winner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    submissions = db.relationship('Submission', backref='challenge')
+    
+class Submission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    poster_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('challenge.id'))
+    path = db.Column(db.Text, nullable=False)
+    dt_submit = db.Column(db.Integer, nullable=False)
+    article_no = db.Column(db.Integer, nullable=False)
+    
