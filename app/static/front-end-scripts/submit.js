64006dabf021f885a9b6abbe44c82730aacedae0
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-04-27 09:23:14 +0800

    dynamically changing button positions

--- app/static/front-end-scripts/submit.js
+++ app/static/front-end-scripts/submit.js
@@ -1,15 +1,37 @@
 
 
+function redoButtons() {
+    const buttons = '<button class="btn btn-primary d-none d-md-block col-0 col-md-1" id="revealPathForm">+</button><button class="btn btn-focus d-none d-md-block col-0 col-md-1" id="removePathForm">-</button>'
+    $("#revealPathForm").remove()
+    $("#removePathForm").remove()
+    $("#path-"+($("#submitForm .path-entry").length-1)).parent().after(buttons);
+    $("#revealPathForm").on("click", revealPath);
+    $("#removePathForm").on("click", removePath);
+    console.log("here")
+}
+
 function revealPath() {
-    const newInput = `<div><input class="path-entry" id="path-${$("#submitForm .path-entry").length}" name="path-${$("#submitForm .path-entry").length}" type="text" value=""></div>`
-    $("#submitForm").append(newInput);
+    const newInput = `<div class="row mb-3 g-0"><div class= "col-12 col-md-10"> <input class="path-entry form-control mx-0" id="path-${$("#submitForm .path-entry").length}" name="path-${$("#submitForm .path-entry").length}" type="text" value=""></div></div>`
+    $("#pathEnd").before(newInput);
+    redoButtons()
+    if ($("#submitForm .path-entry").length >= 2) {
+        $("#removePathForm").show()
+    }
 }
 
 function removePath() {
-    $(".path-entry").last().remove()
+    $(".path-entry").last().parent().parent().remove()
+    redoButtons()
+    if ($("#submitForm .path-entry").length === 1) {
+        $("#removePathForm").hide()
+    }
+
 }
 
 $("document").ready(function() {
-    $("#revealPathForm").on("click", revealPath);
-    $("#removePathForm").on("click", removePath);
+    redoButtons()
+    $("#revealPathFormSmall").on("click", revealPath);
+    $("#removePathFormSmall").on("click", removePath);
+    $("#removePathForm").hide()
+    revealPath();
 })
