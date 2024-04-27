e4f0e1048c310bd435e693c5665a77d74dd13df1
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-04-27 23:23:28 +0800

    made DOMs multline

--- app/static/front-end-scripts/submit.js
+++ app/static/front-end-scripts/submit.js
@@ -1,7 +1,8 @@
 
 
 function redoButtons() {
-    const buttons = '<button type="button" class="btn btn-primary d-none d-md-block col-0 col-md-1" id="revealPathForm">+</button><button type="button" class="btn btn-focus d-none d-md-block col-0 col-md-1" id="removePathForm">-</button>'
+    const buttons = `<button type="button" class="btn btn-primary d-none d-md-block col-0 col-md-1" id="revealPathForm">+</button>
+                    <button type="button" class="btn btn-focus d-none d-md-block col-0 col-md-1" id="removePathForm">-</button>`
     $("#revealPathForm").remove()
     $("#removePathForm").remove()
     $("#path-"+($("#submitForm .path-entry").length-1)).parent().after(buttons);
@@ -11,7 +12,12 @@ function redoButtons() {
 }
 
 function revealPath() {
-    const newInput = `<div class="row mb-3 g-0"><div class= "col-12 col-md-10"> <input class="path-entry form-control mx-0" id="path-${$("#submitForm .path-entry").length}" name="path-${$("#submitForm .path-entry").length}" type="text" value=""></div></div>`
+    const newInput = `
+    <div class="row mb-3 g-0">
+        <div class= "col-12 col-md-10"> 
+            <input class="path-entry form-control mx-0" id="path-${$("#submitForm .path-entry").length}" name="path-${$("#submitForm .path-entry").length}" type="text" value="">
+        </div>
+    </div>`
     $("#pathEnd").before(newInput);
     redoButtons()
 }
