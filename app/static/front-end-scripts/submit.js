a126e6ff55525cf8eecd59aea3cbdcfc4d480b52
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-04-27 23:11:29 +0800

    Ensure buttons don't trigger submit early

--- app/static/front-end-scripts/submit.js
+++ app/static/front-end-scripts/submit.js
@@ -1,7 +1,7 @@
 
 
 function redoButtons() {
-    const buttons = '<button class="btn btn-primary d-none d-md-block col-0 col-md-1" id="revealPathForm">+</button><button class="btn btn-focus d-none d-md-block col-0 col-md-1" id="removePathForm">-</button>'
+    const buttons = '<button type="button" class="btn btn-primary d-none d-md-block col-0 col-md-1" id="revealPathForm">+</button><button type="button" class="btn btn-focus d-none d-md-block col-0 col-md-1" id="removePathForm">-</button>'
     $("#revealPathForm").remove()
     $("#removePathForm").remove()
     $("#path-"+($("#submitForm .path-entry").length-1)).parent().after(buttons);
@@ -14,16 +14,12 @@ function revealPath() {
     const newInput = `<div class="row mb-3 g-0"><div class= "col-12 col-md-10"> <input class="path-entry form-control mx-0" id="path-${$("#submitForm .path-entry").length}" name="path-${$("#submitForm .path-entry").length}" type="text" value=""></div></div>`
     $("#pathEnd").before(newInput);
     redoButtons()
-    if ($("#submitForm .path-entry").length >= 2) {
-        $("#removePathForm").show()
-    }
 }
 
 function removePath() {
-    $(".path-entry").last().parent().parent().remove()
-    redoButtons()
-    if ($("#submitForm .path-entry").length === 1) {
-        $("#removePathForm").hide()
+    if ($("#submitForm .path-entry").length !== 1) {
+        $(".path-entry").last().parent().parent().remove()
+        redoButtons()
     }
 
 }
@@ -32,6 +28,5 @@ $("document").ready(function() {
     redoButtons()
     $("#revealPathFormSmall").on("click", revealPath);
     $("#removePathFormSmall").on("click", removePath);
-    $("#removePathForm").hide()
     revealPath();
 })
