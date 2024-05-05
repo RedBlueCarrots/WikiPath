429ac6279e5c70a7e3af7cbd3d7c7636ac4d5bf3
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-05-06 00:59:02 +0800

    Added multiple wikipedia checks in a single query

--- app/wikipedia.py
+++ app/wikipedia.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from .utilities import *
 
 def checkArticleExists(articleName):
 	if articleName == "":
@@ -7,3 +8,18 @@ def checkArticleExists(articleName):
 	query = requests.get("https://en.wikipedia.org/w/api.php?action=query&titles="+str(articleName)+"&format=json&formatversion=2")
 	queryJson = json.loads(query.text)
 	return not "missing" in queryJson["query"]["pages"][0]
+
+def checkArticlesExists(pathList):
+	pathString = pathify(pathList)
+	query = requests.get("https://en.wikipedia.org/w/api.php?action=query&titles="+pathString+"&format=json&formatversion=2")
+	queryJson = json.loads(query.text)
+	articleInfo = {}
+	#reconvert from normalised to original form
+	if "normalized" in queryJson["query"]:
+		for article in queryJson["query"]["pages"]:
+			for normalised in queryJson["query"]["normalized"]:
+				if normalised["to"] == article["title"]:
+					article["title"] = normalised["from"]
+	for article in queryJson["query"]["pages"]:
+		articleInfo[article["title"]] = not "missing" in article
+	return articleInfo
