5ec6ba9bad96be4d554f16b91407670851226538
RedBlueCarrots <78305845+RedBlueCarrots@users.noreply.github.com>
2024-05-19 09:30:40 +0800

    update front end display

--- app/static/front-end-scripts/submit.js
+++ app/static/front-end-scripts/submit.js
@@ -72,11 +72,11 @@ function addArticleErrors(errorList) {
     })
 }
 
-function addPathErrors(errorList) {
+function addPathErrors(errorList, otherErrorList) {
     let path_entries = $("#pathStart").add($(".path-entry[value!='']")).add($("#pathEnd"));
 
     path_entries.each(function (index) {
-        errorList.includes($(this).attr("value")) && addError($(this).attr("value") + " does not link to " + path_entries.eq(index + 1).attr("value"), $(this).attr("id"));
+        errorList.includes($(this).attr("value")) && !otherErrorList.includes($(this).attr("value")) && addError($(this).attr("value") + " does not link to " + path_entries.eq(index + 1).attr("value"), $(this).attr("id"));
     })
 }
 
@@ -88,5 +88,5 @@ $("document").ready(function () {
     const articleErrorList = $("#submitForm").data("article-errors").split("|");
     const pathErrorList = $("#submitForm").data("path-errors").split("|");
     addArticleErrors(articleErrorList);
-    addPathErrors(pathErrorList);
+    addPathErrors(pathErrorList, articleErrorList);
 })
